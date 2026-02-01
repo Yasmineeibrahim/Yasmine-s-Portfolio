68919cc659ac5e723b63de2e1a1fa5cbd0803d1e
yasmine <yasminee455@gmail.com>
2026-02-01 18:24:01 +0200

    error fixed

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,7 +4,7 @@ import { OrbitControls,Preload,useGLTF } from '@react-three/drei'
 import CanvasLoader from '../Loader'
 
 const Computers = ({isMobile}) => {
-  const computer = useGLTF('./desktop_pc/scene.gltf');
+  const computer = useGLTF('/desktop_pc/scene.gltf');
 
   return (
     <mesh>
@@ -34,12 +34,13 @@ const ComputersCanvas = () => {
 
   return (
     <Canvas
-      frameloop='demand'
+      frameloop='always'
+      dpr={[1, 2]}
       shadows
       camera={{position:[20,3,5],fov:25}}
       gl={{preserveDrawingBuffer:true}}>
       <Suspense fallback={<CanvasLoader/>}>
-        <OrbitControls enableZoom={false} maxPolarAngle={Math.PI/2} minPolarAngle={Math.PI/2}/>
+        <OrbitControls enableZoom={false} maxPolarAngle={Math.PI/2} minPolarAngle={Math.PI/2} makeDefault />
         <Computers isMobile={isMobile}/>
       </Suspense>
       <Preload all/>
